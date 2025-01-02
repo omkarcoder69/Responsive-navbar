4374955f5408b76c67486c34415bc67b413ed884
Omkarwebdev <omkarfakeac123@gmail.com>
2025-01-02 20:26:53 +0530

    Delete navbar directory

--- navbar/script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.querySelector('.menu-icon').addEventListener('click', () => {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('show');
-});
