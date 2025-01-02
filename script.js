8f82ff946c594b17b50982199173510d01e8d16e
Omkarwebdev <omkarfakeac123@gmail.com>
2025-01-02 20:27:34 +0530

    commit  navbar

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('.menu-icon').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+});
